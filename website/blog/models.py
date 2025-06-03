f71c3747162027f1e139149eff1b91c688de3c88
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-03 16:44:53 +0530

    Created 2 models namely Profile and Posts

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    pfp = models.ImageField(default="default.jpg",null=True,blank=True)
+    name = models.CharField()
+    age = models.IntegerField()
+    instagram_id = models.CharField()
+    bio = models.CharField(models.TextField())
+    
+class Posts(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    title = models.CharField()
+    introduction = models.CharField(models.TextField())
+    content = models.CharField(models.TextField())
+    conclusion = models.CharField(models.TextField())
