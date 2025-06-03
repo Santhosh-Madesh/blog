15cddc6fde19eaf2ce5a970f90a192f8fe7a804d
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-03 16:46:04 +0530

    Built views and logics for authentication

---
+++ website/blog/auth.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render,redirect
+from .forms import SignUpForm,LoginForm
+from django.contrib.auth import login,logout, authenticate
+from django.contrib import messages
+from django.contrib.auth.models import User
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            username = form.cleaned_data["username"]
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = User(first_name=first_name,last_name=last_name,username=username,email=email)
+            user.set_password(password)
+            user.save()
+            messages.success(request,"Signed up successfully!")
+            return redirect("login")
+        else:
+            messages.error(request,"Invalid input")
+            return redirect("signup")
+    form = SignUpForm()
+    return render(request,"blog/signup.html",{"form":form})
+
+def login_page(request):
+    if request.user.is_authenticated:
+        messages.warning(request,"You are already logged in, please logout to login with a different account!")
+        return redirect("home")
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username,password=password)
+            if user is None:
+                messages.error(request,"Invalid credentials!")
+                return redirect("login")
+            else:
+                login(request,user)
+                messages.success(request,"Login successful!")
+                return redirect("home")
+    form = LoginForm()
+    return render(request,"blog/login.html",{"form":form})
+
+def logout_page(request):
+    if request.user.is_authenticated:
+        logout(request)
+        messages.success(request,"Logged out successfully!")
+        return redirect("login")
+    else:
+        messages.error(request,"You are not logged in!")
+        return redirect("login")
