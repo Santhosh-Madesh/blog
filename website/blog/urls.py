3687a0b8f11446a8fb52965497a21d370de9fb5a
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-03 16:50:15 +0530

    initialized and connected url patterns to the appropriate views

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from . import views, auth
 
 urlpatterns=[
-
+    path("",views.home,name="home"),
+    path("signup/",auth.signup,name="signup"),
+    path("login/",auth.login_page,name="login"),
+    path("logout/",auth.logout_page,name="logout"),
 ]
