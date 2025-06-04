30940b92806730db806ea237f6cc9172b94dae64
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-04 11:47:31 +0530

    Created url pattern for blog creation view

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -3,6 +3,7 @@ from . import views, auth
 
 urlpatterns=[
     path("",views.home,name="home"),
+    path("blog_creation/",views.blog_creation,name="blog_creation"),
     path("signup/",auth.signup,name="signup"),
     path("login/",auth.login_page,name="login"),
     path("logout/",auth.logout_page,name="logout"),
