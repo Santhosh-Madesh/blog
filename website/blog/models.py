2adc15b5387a716293b06dbac95b2282bae8b1c7
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-04 11:46:10 +0530

    Modified fields of the profile and posts model

--- website/blog/models.py
+++ website/blog/models.py
@@ -1,19 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE,)
     pfp = models.ImageField(default="default.jpg",null=True,blank=True)
     name = models.CharField()
     age = models.IntegerField()
     instagram_id = models.CharField()
-    bio = models.CharField(models.TextField())
+    bio = models.CharField()
     
 class Posts(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.DateField(default=date.today())
     title = models.CharField()
-    introduction = models.CharField(models.TextField())
-    content = models.CharField(models.TextField())
-    conclusion = models.CharField(models.TextField())
+    introduction = models.CharField()
+    content = models.CharField()
+    conclusion = models.CharField()
