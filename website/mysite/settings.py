9f34672deae55d041d0529592f3f46a732c6f2a3
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-05 10:31:51 +0530

    Final commit

--- website/mysite/settings.py
+++ website/mysite/settings.py
@@ -117,11 +117,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = "media/"
+MEDIA_URL = 'media/'
 
 import os
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"blog/media")
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 LOGIN_URL = "/blog/login/"
 
