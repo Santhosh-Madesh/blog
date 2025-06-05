5b41fccec3f9e565dd0e8a174a13fd3a07bce6f4
Santhosh-Madesh <santoshmadesh05@gmail.com>
2025-06-05 09:59:12 +0530

    added url pattern for dashboard

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -8,6 +8,7 @@ urlpatterns=[
     path("delete_blog/<int:pk>",views.delete,name="delete"),
     path("view_blog/<int:pk>",views.view_blog,name="view_blog"),
     path("update_blog/<int:pk>",views.update_blog,name="blog_update"),
+    path("dashboard/",views.dashboard,name="dashboard"),
     path("signup/",auth.signup,name="signup"),
     path("login/",auth.login_page,name="login"),
     path("logout/",auth.logout_page,name="logout"),
